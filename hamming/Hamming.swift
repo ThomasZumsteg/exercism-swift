d866f1ede82ef291a2ab49fb69ea9afb2976fcab
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-09-08 10:32:10 -0700

    Improvments to hamming

--- hamming/Hamming.swift
+++ hamming/Hamming.swift
@@ -3,14 +3,12 @@ import Foundation
 class Hamming {
     class func compute(ref: String, against: String) -> Int? {
         
-        if ref.characters.count != against.characters.count { return nil }
+        let refChars = ref.characters
+        let againstChars = against.characters
         
-        var count = 0
-        for (char_a, char_b) in zip(ref.characters, against.characters) {
-            if char_a != char_b {
-                count++
-            }
-        }
-        return count
+        guard (refChars.count == againstChars.count) else { return nil }
+        
+        let diffs = zip(refChars, againstChars).map{$0 != $1 ? 1 : 0}
+        return diffs.reduce(0, combine: +)
     }
 }
