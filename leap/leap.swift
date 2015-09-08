63c71a54d757d0470f9958ded0c4d915e042c34b
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-09-08 10:16:46 -0700

    leap cleaning variable names

--- leap/leap.swift
+++ leap/leap.swift
@@ -1,12 +1,14 @@
 import Foundation
 
-class Year {
+struct Year {
     var isLeapYear: Bool
     
     init (calendarYear: Int) {
-        let isDivByFour = calendarYear % 4 == 0
-        let isCentury = calendarYear % 100 == 0
-        let isFourCentury = calendarYear % 400 == 0
-        self.isLeapYear = (isDivByFour && !isCentury) || isFourCentury
+        
+        let divBy4   = calendarYear %   4 == 0
+        let divBy100 = calendarYear % 100 == 0
+        let divBy400 = calendarYear % 400 == 0
+        
+        self.isLeapYear = (divBy4 && !divBy100) || divBy400
     }
 }
