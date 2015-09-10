6efaf3f9766402cd1b38ed7d3b0549dabf08378d
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-09-10 10:53:27 -0700

    Added ?? to some functions

--- grade-school/GradeSchool.swift
+++ grade-school/GradeSchool.swift
@@ -5,21 +5,15 @@ typealias School = [Int:[String]]
 class GradeSchool {
     var db: School
     
-    init() {
-        db = School()
-    }
+    init() { db = School() }
     
     
     func addStudent(student: String, grade: Int) {
-        if db[grade] != nil {
-            db[grade]!.append(student)
-        } else {
-            db[grade] = [student]
-        }
+        db[grade] = (db[grade] ?? []) + [student]
     }
     
     func studentsInGrade(grade: Int) -> [String] {
-        return ((db[grade] != nil) ? db[grade]! : [])
+        return db[grade] ?? []
     }
     
     func sortedRoster() -> School {
