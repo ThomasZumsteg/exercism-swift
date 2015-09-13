dc64af23f74b2450ddc1c36ee5daa7b19bac163f
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-09-13 11:31:15 -0700

    First iteration Allergies

---
+++ allergies/AllergiesTest.swift
@@ -0,0 +1,46 @@
+import Foundation
+import XCTest
+
+class AllergiesTest: XCTestCase {
+    
+    func testBob() {
+        
+        let allergies = Allergies(34)
+        
+        XCTAssertTrue(allergies.hasAllergy(Allergies.peanuts), "Bob is allergic to peanuts")
+        XCTAssertTrue(allergies.hasAllergy(Allergies.chocolate), "Bob is allergic to chocolate")
+        XCTAssertFalse(allergies.hasAllergy(Allergies.cats),  "Bob is not allergic to cats")
+    }
+    
+    func testEggsNcats() {
+        
+        let allergies = Allergies(129)
+        
+        XCTAssertTrue(allergies.hasAllergy(Allergies.eggs))
+        XCTAssertTrue(allergies.hasAllergy(Allergies.cats))
+        XCTAssertFalse(allergies.hasAllergy(Allergies.chocolate))
+        
+    }
+    
+    func testNone() {
+        let allergies = Allergies(0)
+        
+        XCTAssertFalse(allergies.hasAllergy(Allergies.pollen))
+    }
+    
+    func testAll() {
+        
+        let allInt =  UInt(Array(0...7).reduce(0){ return ($0 | (1 << $1)) })
+        let allergies = Allergies(allInt)
+        
+        XCTAssertTrue(allergies.hasAllergy(Allergies.eggs))
+        XCTAssertTrue(allergies.hasAllergy(Allergies.peanuts))
+        XCTAssertTrue(allergies.hasAllergy(Allergies.shellfish))
+        XCTAssertTrue(allergies.hasAllergy(Allergies.strawberries))
+        XCTAssertTrue(allergies.hasAllergy(Allergies.tomatoes))
+        XCTAssertTrue(allergies.hasAllergy(Allergies.chocolate))
+        XCTAssertTrue(allergies.hasAllergy(Allergies.pollen))
+        XCTAssertTrue(allergies.hasAllergy(Allergies.cats))
+        
+    }
+}
