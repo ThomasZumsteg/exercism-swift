da8d6603f5cfd4229f0ed774122031319dd5ba38
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-09-22 09:35:55 -0700

    First iteration romain-numerals

---
+++ roman-numerals/RomanNumerals.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+private let romanDigits: [(Int, String)] = [
+    (   1, "I"), (  4, "IV"), (  5, "V"), (  9, "IX"),
+    (  10, "X"), ( 40, "XL"), ( 50, "L"), ( 90, "XC"),
+    ( 100, "C"), (400, "CD"), (500, "D"), (900, "CM"),
+    (1000, "M"),]
+
+extension Int {
+    var toRoman: String {
+        get {
+            var remainer = self
+            var roman = ""
+            for (value, digit) in romanDigits.reverse() {
+                roman += Array(count: remainer / value, repeatedValue: digit).joinWithSeparator("")
+                remainer = remainer % value
+
+            }
+            return roman
+        }
+    }
+}
