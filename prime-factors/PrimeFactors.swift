e831565660d0d5c2c98cb9df8fafa3ddeb8d9347
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-09-24 11:50:10 -0700

    prime-factors now with recursion

--- prime-factors/PrimeFactors.swift
+++ prime-factors/PrimeFactors.swift
@@ -1,17 +1,9 @@
 import Foundation
 
 class PrimeFactors {
-    static func For(num:Int) -> [Int] {
-        var remainer: Int = num
-        var factors: [Int] = [Int]()
-        for f in 2.stride(through: Int(sqrt(Double(num))), by: 1) {
-            if remainer == 1 { break }
-            while remainer % f == 0 {
-                factors.append(f)
-                remainer /= f
-            }
-        }
-        if remainer > 1 { factors.append(remainer) }
-        return factors
+    static func For(num:Int, var _ factor:Int = 2) -> [Int] {
+        if num == 1 { return [] }
+        while (num % factor) != 0 { factor++ }
+        return [factor] + For(num / factor, factor)
     }
 }
