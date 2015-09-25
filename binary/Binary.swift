149a653cea3e7f139adafd361608afc240e8fcc5
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-09-25 10:27:34 -0700

    Small syntax chagne to binary

--- binary/Binary.swift
+++ binary/Binary.swift
@@ -5,9 +5,10 @@ class Binary {
     
     init?(_ bin:String) {
         for c in bin.characters {
+            toDecimal *= 2
             switch c {
-            case Character("0"): toDecimal *= 2
-            case Character("1"): toDecimal = toDecimal * 2 + 1
+            case Character("0"): break
+            case Character("1"): toDecimal++
             default: return nil
             }
         }
