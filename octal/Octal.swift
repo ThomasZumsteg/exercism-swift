5e19160be16591f824a0cdd186774186b635a8bf
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-10-02 10:39:23 -0700

    First iteration octal

---
+++ octal/Octal.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+class Octal {
+    let oct: String
+    
+    init(_ oct: String) {
+        self.oct = oct
+    }
+    
+    var toDecimal: Int {
+        get {
+            var decimal: Int = 0
+            for n in oct.characters.map({Int(String($0))}) {
+                guard n != nil && n! < 8 else { return 0 }
+                decimal = decimal * 8 + n!
+            }
+            return decimal
+        }
+    }
+}
