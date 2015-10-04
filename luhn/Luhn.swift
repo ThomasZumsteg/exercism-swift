34fb06c19e0fb29f6914ce045dbe2df5571b289b
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-10-03 22:41:43 -0700

    First iteration luhn

--- luhn/Luhn.swift
+++ luhn/Luhn.swift
@@ -6,18 +6,40 @@ class Luhn {
     init(_ code: Int) { self.code = code }
     
     var addends: [Int] {
-        get{ return [1,4,1,4,1] }
+        get {
+            var odd = false
+            var ends = [Int]()
+            let digits: [Int] = String(code)
+                .characters
+                .reverse()
+                .map{Int(String($0))!}
+            for var n in digits {
+                if odd { n *= 2 }
+                if 10 <= n { n -= 9 }
+                odd = !odd
+                ends.insert(n, atIndex: 0)
+            }
+            return ends
+        }
     }
     
     var checksum: Int {
-        get { return 22 }
+        get {
+            return addends.reduce(0, combine: +)
+        }
     }
     
     var isValid: Bool {
-        get { return false }
+        get {
+            return 0 == (checksum % 10)
+        }
     }
     
     static func create(base: Int) -> Int {
-        return 1230
+        let baseLuhn: Luhn = Luhn(base * 10)
+        if baseLuhn.checksum % 10 == 0 { return baseLuhn.code }
+        else {
+            return baseLuhn.code + 10 - (baseLuhn.checksum % 10)
+        }
     }
 }
