dbf39246c0763f416564662aac26a61808d0063f
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2015-10-14 11:30:38 -0700

    First iteration wordy

--- wordy/Wordy.swift
+++ wordy/Wordy.swift
@@ -2,31 +2,39 @@ import Foundation
 
 class WordProblem {
     let phrase: String
-    let operations: [String: (Int, Int) -> Int] = [
-        "minus": { $0 - $1 },
-        "plus": { $0 + $1 },
-        "multiplied by": { $0 * $1 },
-        "divided by": { $0 / $1 },
-    ]
     
     init(_ phrase: String) {
         self.phrase = phrase
     }
     
     func answer() throws -> Int {
-        guard phrase.hasPrefix("What is ") else { throw answerError.error }
-        // Get the first digit
-        
-        
-        let stepRegexp = try! NSRegularExpression(" (plus|minus|multiplied by|divided by) (-?\\d+)", options: [])
-        // for a receding range, while there's a match,
-        // process the match
-            let num: Int = Int()
-            let op: String = (phrase as NSString).substringWithRange(match[0].rangeAtIndex(1))
-        // update the result
-        
-        // check that the remaining character is a "?"
-        // return result
+        let pattern: String = "(^What is| plus| minus| multiplied by| divided by) (-?\\d+)"
+        let stepRegexp = try! NSRegularExpression(pattern: pattern, options: [])
+        let wholePhrase: NSRange = NSMakeRange(0, phrase.characters.count)
+        let step = stepRegexp.matchesInString(phrase, options: [], range: wholePhrase)
+        guard step.count >= 2 &&
+            (phrase as NSString).substringWithRange(step[0].rangeAtIndex(1)) == "What is"
+            else { throw answerError.error }
+        var result: Int = 0
+        for s in 0..<step.count {
+            let op = (phrase as NSString).substringWithRange(step[s].rangeAtIndex(1))
+            let num = (phrase as NSString).substringWithRange(step[s].rangeAtIndex(2))
+            if let n = Int(num) {
+                result = try stepFunc(result, op: op, num: n)
+            }
+        }
+        return result
+    }
+    
+    private func stepFunc(inital: Int, op: String, num: Int) throws -> Int {
+        switch op {
+        case "What is": return num
+        case " plus": return inital + num
+        case " minus": return inital - num
+        case " multiplied by": return inital * num
+        case " divided by": return inital / num
+        default: throw answerError.error
+        }
     }
 }
 
