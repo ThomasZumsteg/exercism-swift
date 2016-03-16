70f30bb0be435186ca30815fa1eeef7c5d772a75
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-03-16 09:55:31 -0700

    Updated phone-number to match new tests
    changed number and area code to getter types
    Added string extension to make pretty phone number

--- phone-number/phone-number.swift
+++ phone-number/phone-number.swift
@@ -4,7 +4,7 @@ class PhoneNumber {
     var digits: [Character]
     let numbers = NSCharacterSet.decimalDigitCharacterSet()
     
-    init (startingNumber: String) {
+    init (_ startingNumber: String) {
         let utfDigits = startingNumber.utf16.filter(numbers.characterIsMember)
         digits = utfDigits.map{Character(UnicodeScalar($0))}
         
@@ -15,20 +15,30 @@ class PhoneNumber {
         }
     }
     
-    func number() -> String {
-        return String(digits)
+    var number: String {
+        get {
+            return String(digits)
+        }
     }
     
-    func areaCode() -> String {
-        return String(digits[0..<3])
+    var areaCode: String {
+        get {
+            return String(digits[0..<3])
+        }
     }
     
-    func description() -> String {
-        let areaCode = self.areaCode()
+    private func toString() -> String {
+        let areaCode = self.areaCode
         let prefix = String(digits[3..<6])
         let lineNum = String(digits[6..<10])
         
         return "(\(areaCode)) \(prefix)-\(lineNum)"
     }
     
+}
+
+extension String {
+    init(_ phonenumber: PhoneNumber) {
+            self.init(phonenumber.toString())
+    }
 }
