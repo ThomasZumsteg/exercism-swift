87f28f71351c5b39efeca125c83361773e9ab2f1
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-03-17 09:46:43 -0700

    Updated roman-numerals to match new unit tests
    Made a class from the toRoman method
    Extended string class to convert from roman numerals to strings

--- roman-numerals/RomanNumerals.swift
+++ roman-numerals/RomanNumerals.swift
@@ -1,23 +1,32 @@
 import Foundation
 
-private let romanDigits: [(Int, String)] = [
-    (   1, "I"), (  4, "IV"), (  5, "V"), (  9, "IX"),
-    (  10, "X"), ( 40, "XL"), ( 50, "L"), ( 90, "XC"),
-    ( 100, "C"), (400, "CD"), (500, "D"), (900, "CM"),
-    (1000, "M"),]
+class RomanNumeral {
+    var decimal: Int
+    let romanDigits: [(Int, String)] = [
+        (   1, "I"), (  4, "IV"), (  5, "V"), (  9, "IX"),
+        (  10, "X"), ( 40, "XL"), ( 50, "L"), ( 90, "XC"),
+        ( 100, "C"), (400, "CD"), (500, "D"), (900, "CM"),
+        (1000, "M"),]
 
-extension Int {
-    var toRoman: String {
-        get {
-            var remainer = self
-            var roman = ""
-            for (value, digit) in romanDigits.reverse() {
-                while value <= remainer {
-                    roman += digit
-                    remainer -= value
-                }
+    init(_ decimal: Int) {
+        self.decimal = decimal
+    }
+    
+    private func toString() -> String {
+        var remainer = self.decimal
+        var roman = ""
+        for (value, digit) in romanDigits.reverse() {
+            while value <= remainer {
+                roman += digit
+                remainer -= value
             }
-            return roman
         }
+        return roman
+    }
+}
+
+extension String {
+    init(_ roman: RomanNumeral) {
+        self.init(roman.toString())
     }
 }
