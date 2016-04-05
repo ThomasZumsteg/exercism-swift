2c6a63e823a810340119705924a14761ff57be22
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-04-05 16:42:20 -0700

    Still working on poker

--- poker/Poker.swift
+++ poker/Poker.swift
@@ -2,56 +2,47 @@ import Foundation
 
 class Poker {
     static func bestHand(hands: [String]) -> String {
-        var best: PokerHand? = nil
-        for hand in hands.map({ PokerHand($0) }) {
-            if best == nil || (hand != nil && best! < hand!) {
-                best = hand
-            }
-        }
-        return best!.hand
+        return hands
+            .map({PokerHand($0)})
+            .maxElement(<)!!
+            .hand
     }
+}
+
+struct Card {
+    let value, suit: Int
 }
 
 class PokerHand {
     let hand: String
-    var suits = Set<String>()
-    var ranks = [Int:Int]()
+    var cards = [Card]()
     
     enum Error: ErrorType {
         case InvalidSuit, InvalidRank
     }
-    
-    enum Catagory: Int {
+
+    enum Rank: Int {
         case HighCard = 0, OnePair, TwoPair, ThreeOfAKind, Straight, Flush, FullHouse, FourOfAKind, StraighFlush
     }
     
     init?(_ hand: String) {
         self.hand = hand
-        let cards = hand.characters.split{ $0 == " " }
+        do {
+            self.cards = try hand
+                .componentsSeparatedByString(" ")
+                .map({Card(
+                    value: try getValue($0),
+                    suit: try getSuit($0))
+                })
+        } catch {
+            return nil
+        }
         if cards.count != 5 {
             return nil
         }
-        for card in cards.map({ String($0) }) {
-            do {
-                let suit = try getSuit(card)
-                let rank = try getRank(card)
-                suits.insert(suit)
-                ranks[rank] = (ranks[rank] ?? 0) + 1
-            } catch {
-                return nil
-            }
-        }
     }
     
-    private func getSuit(card: String) throws -> String {
-        let suit = card.substringFromIndex(card.endIndex.predecessor())
-        if !["♡", "♢", "♧", "♤"].contains(suit) {
-            throw Error.InvalidSuit
-        }
-        return suit
-    }
-    
-    private func getRank(card: String) throws -> Int {
+    func getValue(card: String) throws -> Int {
         let rank = card.substringToIndex(card.endIndex.predecessor())
         switch rank {
         case "2", "3", "4", "5", "6", "7", "8", "9", "10":
@@ -69,41 +60,73 @@ class PokerHand {
         }
     }
     
+    func getSuit(card: String) throws -> Int {
+        let suit = card.substringFromIndex(card.endIndex.predecessor())
+        if let suitScore = ["♡", "♢", "♧", "♤"].indexOf(suit) {
+            return suitScore
+        }
+        throw Error.InvalidSuit
+    }
+
     private func isFlush() -> Bool {
-        return suits.count == 1
+        return cards
+            .reduce(true){$0 && $1.suit == cards[0].suit}
     }
     
     private func isStraight() -> Bool {
+        let ranks = cards.map({$0.value})
+        if ranks == [2,3,4,5,14] {
+            return true
+        }
+        for var i = 1; i < ranks.count; i++ {
+            if ranks[i-1] + 1 != ranks[i] {
+                return false
+            }
+        }
+        return true
+    }
+    
+    private func getRank() -> Int {
+        let cardGroups = [1,1,1,1,1]
+        
+        switch true {
+        case isFlush() && isStraight():
+            return Rank.StraighFlush.rawValue
+        case cardGroups == [1,4]:
+            return Rank.FourOfAKind.rawValue
+        case cardGroups == [2,3]:
+            return Rank.FullHouse.rawValue
+        case isFlush() && !isStraight():
+            return Rank.Flush.rawValue
+        case !isFlush() && isStraight():
+            return Rank.Straight.rawValue
+        case cardGroups == [1,1,3]:
+            return Rank.ThreeOfAKind.rawValue
+        case cardGroups == [1,2,2]:
+            return Rank.TwoPair.rawValue
+        case cardGroups == [1,1,1,2]:
+            return Rank.OnePair.rawValue
+        default:
+            return Rank.HighCard.rawValue
+        }
+   }
+
+}
+
+func <(lhs: PokerHand?, rhs: PokerHand?) -> Bool {
+    guard let leftHand = lhs else {
+        return true
+    }
+    guard let rightHand = rhs else {
         return false
     }
     
-    private func rankGroups() -> [Int] {
-        return ranks.keys.sort()
+    if leftHand.getRank() != rightHand.getRank() {
+        return leftHand.getRank() < rightHand.getRank()
     }
     
-    private var catagory: Catagory {
-        switch true {
-        case isFlush() && isStraight(): return Catagory.StraighFlush
-        case rankGroups() == [4,1]:
-            return Catagory.FourOfAKind
-        case rankGroups() == [3,2]:
-            return Catagory.FullHouse
-        case isFlush() && !isStraight():
-            return Catagory.Flush
-        case !isFlush() && isStraight():
-            return Catagory.Straight
-        case rankGroups() == [3,1,1]:
-            return Catagory.ThreeOfAKind
-        case rankGroups() == [2,2,1]:
-            return Catagory.TwoPair
-        case rankGroups() == [2,1,1,1]:
-            return Catagory.OnePair
-        default:
-            return Catagory.HighCard
-        }
-    }
-}
+    for (leftCard, rightCard) in zip(leftHand.cardGroups(), rightHand.cardGroups()) {
 
-func <(lhs: PokerHand, rhs: PokerHand) -> Bool {
-    return lhs.catagory.rawValue < rhs.catagory.rawValue
+    }
+    return true
 }
