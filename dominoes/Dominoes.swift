b469d1ce8a6d2f1f502b2cd1e260b3ff44a7afae
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-04-12 09:57:41 -0700

    Work on the queue

--- dominoes/Dominoes.swift
+++ dominoes/Dominoes.swift
@@ -6,25 +6,41 @@ class Dominoes {
         dominoes = chain.map{Dominoe($0.0, $0.1)}
     }
     
+    typealias Chain = [(Int, Int)]
+    typealias DominoeState = (Chain, [Dominoe], Int)
     var chained: Bool {
-        var queue = [([(Int, Int)](), [Dominoe](), Int)]
+        var queue = [DominoeState]()
+        
+        for (i, dominoe) in dominoes.enumerate() {
+            let newPool = dominoes.prefix(i) + dominoes.suffixFrom(i + 1)
+            queue.append((
+                [(dominoe.left, dominoe.right)],
+                Array(newPool),
+                dominoe.right))
+            queue.append((
+                [(dominoe.right, dominoe.left)],
+                Array(newPool),
+                dominoe.left))
+        }
         
         while !queue.isEmpty {
             let (chain, pool, tail) = queue.popLast()!
-            if pool.isEmpty && chain.first!.0 == chain.last!.1 {
+            if pool.isEmpty && !chain.isEmpty &&
+                chain.first!.0 == chain.last!.1 {
                 return true
             }
             
-            for i in (0...pool.count) {
+            for i in (0..<pool.count) {
                 if let match = pool[i].hasNum(tail) {
                     let newPool = pool[0..<i] + pool[i..<pool.count]
                     let newChain = chain + [match]
                     let newTail = match.1
-                    queue.append((newTail, newPool, newTail))
+                    queue.append((newChain, Array(newPool), newTail))
                 }
             }
             
         }
+        return false
     }
 }
 
